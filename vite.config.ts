7b86b4e4f3617bd2c2c986b57cfd54fdb925f28c
Veikko M. <veikkosakari01@gmail.com>
2024-07-14 19:50:03 +0300

    Added more assets in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/portfolio-page/',
-  assetsInclude: ["**/*.gltf"]
+  assetsInclude: ["**/*.gltf", "**/*.svg","**/*.png", "**/*.jpg"]
 })
